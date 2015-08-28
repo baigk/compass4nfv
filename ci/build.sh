2bc59c67cecda1770c64b904064d1be3e432813a
carey.xu <carey.xuhan@huawei.com>
2015-08-28 09:41:33 -0700

    add used_nics

--- ci/build.sh
+++ ci/build.sh
@@ -72,8 +72,8 @@ function download_packages()
 
      done
 
-     git fetch
-     git checkout origin/master -- $COMPASS_DIR/deploy/adapters
+     #git fetch
+     #git checkout origin/master -- $COMPASS_DIR/deploy/adapters
 }
 
 function copy_file()

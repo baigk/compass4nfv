6a4c30a141e4ef1adda80953f39e18269443f1d1
baigk <baiguoku@huawei.com>
2015-09-01 14:33:00 +0800

    support to specify the version of package or source packaging when building ppa

--- ci/make_repo.sh
+++ ci/make_repo.sh
@@ -2,98 +2,116 @@
 set -ex
 
 WORK_PATH=$(cd "$(dirname "$0")"; pwd)
-REPO_PATH=${WORK_PATH}/repo
-UBUNTU_TAG="trusty"
-CENTOS_TAG="centos7"
-OPENSTACK_TAG="juno"
-DOCKER_TAG="${UBUNTU_TAG}/openstack-${OPENSTACK_TAG}"
-DOCKER_FILE=${WORK_PATH}/${UBUNTU_TAG}/${OPENSTACK_TAG}/Dockerfile
-DEPLOY_SCRIPT_PATH="$1"
 
-if [[ ! -d ${REPO_PATH} ]]; then
-    mkdir -p ${REPO_PATH}
-fi
+function process_env()
+{
+    mkdir -p ${WORK_PATH}/work/repo/
 
-set +e
-sudo docker info
-if [[ $? != 0 ]]; then
-    wget -qO- https://get.docker.com/ | sh
-else
-    echo "docker is already installed!"
-fi
-set -e
+    set +e
+    sudo docker info
+    if [[ $? != 0 ]]; then
+        wget -qO- https://get.docker.com/ | sh
+    else
+        echo "docker is already installed!"
+    fi
+    set -e
 
-if [[ -e ${WORK_PATH}/cp_repo.sh ]]; then
-    rm -f ${WORK_PATH}/cp_repo.sh
-fi
-
-cat <<EOF >${WORK_PATH}/cp_repo.sh
+cat <<EOF >${WORK_PATH}/work/repo/cp_repo.sh
 #!/bin/bash
 set -ex
 cp /*ppa.tar.gz /result
 EOF
 
-if [[ -e ${WORK_PATH}/install_packeages.sh ]]; then
-    rm -f ${WORK_PATH}/install_packages.sh
+    sudo apt-get install python-yaml -y
+    sudo apt-get install python-cheetah -y
+}
+
+function make_repo()
+{
+    rm -f ${WORK_PATH}/work/repo/install_packages.sh
+    rm -f ${WORK_PATH}/work/repo/Dockerfile
+
+    TEMP=`getopt -o h -l os-tag:,openstack-tag:,tmpl:,default-package:,special-package:,ansible-dir: -n 'make_repo.sh' -- "$@"`
+
+    if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+    eval set -- "$TEMP"
+
+    os_tag=""
+    openstack_tag=""
+    tmpl=""
+    default_package=""
+    special_package=""
+    special_package_dir=""
+    ansible_dir=""
+    while :; do
+        case "$1" in
+            --os-tag) os_tag=$2; shift 2;;
+            --openstack-tag) openstack_tag=$2; shift 2;;
+            --tmpl) tmpl=$2; shift 2;;
+            --default-package) default_package=$2; shift 2;;
+            --special-package) special_package=$2; shift 2;;
+            --special-package-dir) special_package_dir=$2; shift 2;;
+            --ansible-dir) ansible_dir=$2; shift 2;;
+            --) shift; break;;
+            *) echo "Internal error!" ; exit 1 ;;
+        esac
+    done
+
+    if [[ -z ${os_tag} || -z ${openstack_tag} || -z ${tmpl} || -z ${ansible_dir} ]]; then
+        echo "parameter is wrong"
+        exit 1
+    fi
+
+    if [[ ${os_tag} == "trusty" ]]; then
+        arch=Debian
+    fi
+
+    if [[ ${os_tag} == "centos7" ]]; then
+        arch=RedHat
+    fi
+
+    dockerfile=Dockerfile
+    docker_tmpl=${WORK_PATH}/${os_tag}/${openstack_tag}/${dockerfile}".tmpl"
+    docker_tag="${os_tag}/openstack-${openstack_tag}"
+
+    python gen_ins_pkg_script.py ${ansible_dir} ${arch} ${tmpl} \
+               ${docker_tmpl} "${default_package}" "${special_package}" "${special_package_dir}"
+
+    sudo docker build -t ${docker_tag} -f ${WORK_PATH}/work/repo/${dockerfile} .
+
+    sudo docker run -t -v ${WORK_PATH}/work/repo:/result ${docker_tag}
+
+    image_id=$(sudo docker images|grep ${docker_tag}|awk '{print $3}')
+
+    sudo docker rmi -f ${image_id}
+}
+
+function make_all_repo()
+{
+    make_repo --os-tag trusty --openstack-tag juno \
+              --ansible-dir $WORK_PATH/../deploy/adapters/ansible \
+              --tmpl Debian_juno.tmpl \
+              --default-package "openssh-server" \
+              --special-package "openvswitch-datapath-dkms openvswitch-switch"
+    if false; then
+    make_repo --os-tag trusty --openstack-tag kilo \
+              --ansible-dir $WORK_PATH/../deploy/adapters/ansible \
+              --tmpl Debian_kilo.tmpl \
+              --default-package "openssh-server" \
+              --special-package "openvswitch-datapath-dkms openvswitch-switch"
+    fi
+    make_repo --os-tag centos7 --openstack-tag juno \
+              --ansible-dir $WORK_PATH/../deploy/adapters/ansible \
+              --tmpl RedHat_juno.tmpl \
+              --default-package "strace net-tools wget vim openssh-server dracut-config-rescue dracut-network" \
+              --special-package ""
+}
+
+process_env
+
+if [[ $# -eq 0 ]]; then
+    make_all_repo
+else
+    make_repo $*
 fi
-
-sudo apt-get install python-yaml -y
-sudo apt-get install python-cheetah -y
-
-# generate ubuntu 14.04 juno ppa
-python gen_ins_pkg_script.py ${DEPLOY_SCRIPT_PATH} Debian Debian_juno.tmpl
-
-sudo docker build -t ${DOCKER_TAG} -f ${DOCKER_FILE} .
-
-mkdir -p ${REPO_PATH}
-sudo docker run -t -v ${REPO_PATH}:/result ${DOCKER_TAG}
-
-IMAGE_ID=$(sudo docker images|grep ${DOCKER_TAG}|awk '{print $3}')
-sudo docker rmi -f ${IMAGE_ID}
-
-if [[ -e ${WORK_PATH}/install_packages.sh ]]; then
-    rm -f ${WORK_PATH}/install_packages.sh
-fi
-
-# generate ubuntu 14.04 kilo ppa
-python gen_ins_pkg_script.py ${DEPLOY_SCRIPT_PATH} Debian Debian_kilo.tmpl
-
-OPENSTACK_TAG="kilo"
-DOCKER_TAG="${UBUNTU_TAG}/openstack-${OPENSTACK_TAG}"
-DOCKER_FILE=${WORK_PATH}/${UBUNTU_TAG}/${OPENSTACK_TAG}/Dockerfile
-
-sudo docker build -t ${DOCKER_TAG} -f ${DOCKER_FILE} .
-
-mkdir -p ${REPO_PATH}
-sudo docker run -t -v ${REPO_PATH}:/result ${DOCKER_TAG}
-
-IMAGE_ID=$(sudo docker images|grep ${DOCKER_TAG}|awk '{print $3}')
-sudo docker rmi -f ${IMAGE_ID}
-
-if [[ -e ${WORK_PATH}/install_packages.sh ]]; then
-    rm -f ${WORK_PATH}/install_packages.sh
-fi
-
-# generate centos 7.1 juno ppa
-python gen_ins_pkg_script.py ${DEPLOY_SCRIPT_PATH} RedHat RedHat_juno.tmpl
-
-OPENSTACK_TAG="juno"
-DOCKER_TAG="${CENTOS_TAG}/openstack-${OPENSTACK_TAG}"
-DOCKER_FILE=${WORK_PATH}/${CENTOS_TAG}/${OPENSTACK_TAG}/Dockerfile
-
-sudo docker build -t ${DOCKER_TAG} -f ${DOCKER_FILE} .
-
-mkdir -p ${REPO_PATH}
-sudo docker run -t -v ${REPO_PATH}:/result ${DOCKER_TAG}
-
-IMAGE_ID=$(sudo docker images|grep ${DOCKER_TAG}|awk '{print $3}')
-sudo docker rmi -f ${IMAGE_ID}
-
-if [[ -e ${WORK_PATH}/install_packages.sh ]]; then
-    rm -f ${WORK_PATH}/install_packages.sh
-fi
-
-if [[ -e ${WORK_PATH}/cp_repo.sh ]]; then
-    rm -f ${WORK_PATH}/cp_repo.sh
-fi
-

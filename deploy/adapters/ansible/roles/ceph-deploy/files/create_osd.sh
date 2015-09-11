933feaedfcf45fda2c1631ab8c579625149592c4
grakiss <grakiss.wanglei@huawei.com>
2015-09-11 08:35:27 +0800

    fix ceph deploy

--- deploy/adapters/ansible/roles/ceph-deploy/files/create_osd.sh
+++ deploy/adapters/ansible/roles/ceph-deploy/files/create_osd.sh
@@ -1,17 +1,25 @@
-mkdir -p /ceph/images
-
-if [ ! -f "/ceph/images.ceph-volumes.img"]; then 
-dd if=/dev/zero of=/ceph/images/ceph-volumes.img bs=1M seek=10240 count=0 oflag=direct
-sgdisk -g --clear /ceph/images/ceph-volumes.img
-vgcreate ceph-volumes $(sudo losetup --show -f /ceph/images/ceph-volumes.img)
-sudo lvcreate -L9G -nceph0 ceph-volumes
+if [ -d "/var/local/osd" ]; then
+rm -r /var/local/osd/*
+umount /var/local/osd
+rm -r /var/local/osd
 fi
 
-if [ ! -f "/dev/ceph-volumes/ceph0"]; then
+if [ ! -d "/ceph/images" ]; then
+mkdir -p /ceph/images
+fi
+
+if [ ! -f "/ceph/images/ceph-volumes.img" ]; then 
+dd if=/dev/zero of=/ceph/images/ceph-volumes.img bs=1M seek=10240 count=0 oflag=direct
+sgdisk -g --clear /ceph/images/ceph-volumes.img
+fi
+
+if [ ! -L "/dev/ceph-volumes/ceph0" ]; then
+vgcreate ceph-volumes $(sudo losetup --show -f /ceph/images/ceph-volumes.img)
+sudo lvcreate -L9G -nceph0 ceph-volumes
 mkfs.xfs -f /dev/ceph-volumes/ceph0
 fi
 
-if [ ! -f "/var/local/osd"]; then
+if [ ! -d "/var/local/osd" ]; then
 mkdir -p /var/local/osd
 mount /dev/ceph-volumes/ceph0 /var/local/osd
 fi
